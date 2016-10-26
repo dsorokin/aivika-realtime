f88f4ce506418c88c647be16cafc4deacea23b99
David Sorokin <david.sorokin@gmail.com>
2016-10-26 17:58:57 +0300

    Updated module RT and added tests.

--- Simulation/Aivika/RealTime/RT.hs
+++ Simulation/Aivika/RealTime/RT.hs
@@ -10,7 +10,8 @@
 -- This module defines a soft real-time computation based on 'IO'.
 --
 module Simulation.Aivika.RealTime.RT
-       (RT,
+       (-- * Soft real-time computation
+        RT,
         RTParams(..),
         RTContext,
         RTScaling(..),
@@ -18,10 +19,18 @@ module Simulation.Aivika.RealTime.RT
         defaultRTParams,
         newRTContext,
         rtParams,
-        rtScale) where
+        rtScale,
+        -- * Invoking actions within the simulation
+        applyEventRT,
+        applyEventRT_,
+        enqueueEventRT,
+        enqueueEventRT_) where
 
 import Control.Monad
 import Control.Monad.Trans
+
+import Control.Concurrent.STM
+import Control.Concurrent.Async
 
 import Simulation.Aivika.Trans
 import Simulation.Aivika.IO.Comp
@@ -30,6 +39,7 @@ import Simulation.Aivika.IO.QueueStrategy
 import Simulation.Aivika.IO.Exception
 
 import Simulation.Aivika.RealTime.Internal.RT
+import Simulation.Aivika.RealTime.Internal.Channel
 import Simulation.Aivika.RealTime.Event
 
 -- | An implementation of the 'MonadTemplate' type class.
@@ -46,4 +56,72 @@ instance (Monad m, MonadIO m, MonadException m) => EventIOQueueing (RT m) where
   {-# SPECIALIZE instance EventIOQueueing (RT IO) #-}
 
   enqueueEventIO = enqueueEvent
-  
+
+-- | Invoke the action within the soft real-time simulation.
+invokeEventRT_ :: MonadIO m
+                  => RTContext m
+                  -- ^ the computation context
+                  -> (Event (RT m) () -> Event (RT m) ())
+                  -- ^ the computation transform
+                  -> Event (RT m) ()
+                  -- ^ the computation to invoke
+                  -> m ()
+                  -- ^ the action of invoking the computation
+{-# INLINABLE invokeEventRT_ #-}
+invokeEventRT_ ctx f m =
+  let ch = rtChannel0 ctx
+  in liftIO $ writeChannel ch $ f m
+
+-- | Invoke the action within the soft real-time simulation.
+invokeEventRT :: MonadIO m
+                 => RTContext m
+                 -- ^ the computation context
+                 -> (Event (RT m) () -> Event (RT m) ())
+                 -- ^ the computation transform
+                 -> Event (RT m) a
+                 -- ^ the computation to invoke
+                 -> m (Async a)
+                 -- ^ the result of computation
+{-# INLINABLE invokeEventRT #-}
+invokeEventRT ctx f m =
+  do let ch = rtChannel0 ctx
+     v <- liftIO $ newTVarIO Nothing
+     liftIO $
+       writeChannel ch $
+       f $
+       do a <- m
+          liftIO $
+            atomically $
+            writeTVar v (Just a)
+     liftIO $
+       async $
+       atomically $
+       do b <- readTVar v
+          case b of
+            Just a -> return a
+            Nothing -> retry
+
+-- | Apply the 'Event' computation within the soft real-time simulation
+-- with the specified context and return the result.
+applyEventRT :: MonadIO m => RTContext m -> Event (RT m) a -> m (Async a)
+{-# INLINABLE applyEventRT #-}
+applyEventRT ctx m = invokeEventRT ctx id m
+
+-- | Apply the 'Event' computation within the soft real-time simulation
+-- with the specified context.
+applyEventRT_ :: MonadIO m => RTContext m -> Event (RT m) () -> m ()
+{-# INLINABLE applyEventRT_ #-}
+applyEventRT_ ctx m = invokeEventRT_ ctx id m
+
+-- | Enqueue the 'Event' computation within the soft real-time simulation
+-- with the specified context at the modeling time provided and
+-- then return the result.
+enqueueEventRT :: MonadIO m => RTContext m -> Double -> Event (RT m) a -> m (Async a)
+{-# INLINABLE enqueueEventRT #-}
+enqueueEventRT ctx t m = invokeEventRT ctx (enqueueEvent t) m
+
+-- | Enqueue the 'Event' computation within the soft real-time simulation
+-- with the specified context at the modeling time provided.
+enqueueEventRT_ :: MonadIO m => RTContext m -> Double -> Event (RT m) () -> m ()
+{-# INLINABLE enqueueEventRT_ #-}
+enqueueEventRT_ ctx t m = invokeEventRT_ ctx (enqueueEvent t) m
---
+++ tests/MachRep1Ver3.hs
@@ -0,0 +1,91 @@
+
+-- It corresponds to model MachRep1 described in document 
+-- Introduction to Discrete-Event Simulation and the SimPy Language
+-- [http://heather.cs.ucdavis.edu/~matloff/156/PLN/DESimIntro.pdf]. 
+-- SimPy is available on [http://simpy.sourceforge.net/].
+--   
+-- The model description is as follows.
+--
+-- Two machines, which sometimes break down.
+-- Up time is exponentially distributed with mean 1.0, and repair time is
+-- exponentially distributed with mean 0.5. There are two repairpersons,
+-- so the two machines can be repaired simultaneously if they are down
+-- at the same time.
+--
+-- Output is long-run proportion of up time. Should get value of about
+-- 0.66.
+
+import Control.Monad.Trans
+import Control.Concurrent
+import Control.Concurrent.Async
+
+import Data.Time.Clock
+
+import Simulation.Aivika.Trans
+import Simulation.Aivika.RealTime
+
+type DES = RT IO
+
+meanUpTime = 1.0
+meanRepairTime = 0.5
+
+specs = Specs { spcStartTime = 0.0,
+                -- spcStopTime = 1000.0,
+                spcStopTime = 30.0,
+                spcDT = 1.0,
+                spcMethod = RungeKutta4,
+                spcGeneratorType = SimpleGenerator }
+        
+model :: Simulation DES (Results DES)
+model =
+  do totalUpTime <- newRef 0.0
+     
+     let machine =
+           do upTime <-
+                liftParameter $
+                randomExponential meanUpTime
+              holdProcess upTime
+              liftEvent $ 
+                modifyRef totalUpTime (+ upTime)
+              repairTime <-
+                liftParameter $
+                randomExponential meanRepairTime
+              holdProcess repairTime
+              machine
+
+     runEventInStartTime $
+       enqueueEventWithIntegTimes $
+       do utc <- liftIO getCurrentTime
+          traceEvent ("current time: " ++ show utc) $
+            return ()
+
+     runProcessInStartTime machine
+     runProcessInStartTime machine
+
+     let upTimeProp =
+           do x <- readRef totalUpTime
+              y <- liftDynamics time
+              return $ x / (2 * y)
+
+     return $
+       results
+       [resultSource
+        "upTimeProp"
+        "The long-run proportion of up time (~ 0.66)"
+        upTimeProp]
+  
+main =
+  do let rt = printSimulationResultsInStopTime
+              printResultSourceInEnglish
+              model specs
+     ctx <- newRTContext $ defaultRTParams { rtScaling = RTLinearScaling 1 }
+     forkIO $
+       do threadDelay (round $ 15.5 * 1000000)
+          putStrLn "Invoking the action (after 15.5 sec.)"
+          future <-
+            applyEventRT ctx $
+            traceEvent "The action was invoked" $
+            liftDynamics time
+          t <- wait future
+          putStrLn $ "The time of invocation t = " ++ show t
+     runRT rt ctx
